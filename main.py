2c87dc47fdd78a7657616554975d9c1016966952
IlyaKha <powerfulkha96@mail.ru>
2024-05-16 22:08:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+
+# Задание 1
+text = "Сейчас на Земле появился новый вид роботов. Раньше их называли „железной оравой “, но это не очень точное определение»"
+print(text[:43])
+# Задание 2
+name = "Радар"
+print(name[::-1])
+# Задание 3
+name1 = "Кенгуру"
+middle = len(name1) // 2
+first = name1[:middle]
+second = name1[middle:]
+result = second + first
+print(result)
+# Задание 4
+text_one = "нейропрограммирование"
+first = text_one[1::2]
+second = text_one[::2]
+result = second + first
+print(result)
+# Задание 5
+word = "нейропластичность"
+print(word[-2:0:-1])
